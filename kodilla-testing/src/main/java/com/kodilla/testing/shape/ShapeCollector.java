3742061842fa4d8da7b5df223fea53a9485d8f58
MichalPalonka <michalpalonka@gmail.com>
2019-04-23 22:11:58 +0200

    Zadanie 6.5

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,7 +1,7 @@
 package com.kodilla.testing.shape;
 
-import java.util.ArrayList;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class ShapeCollector {
     private List<Shape> shapeList= new ArrayList<>();
