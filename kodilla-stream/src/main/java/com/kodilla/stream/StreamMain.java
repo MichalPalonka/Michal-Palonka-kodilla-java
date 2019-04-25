1eac859a1a60d7c0045eb7b5a9373f91c3c418fa
MichalPalonka <michalpalonka@gmail.com>
2019-04-25 23:23:50 +0200

    Zad 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -31,5 +32,8 @@ public class StreamMain {
         System.out.println(testString3);
         String testString4 = poemBeautifier.beautify("Test text to test task.", text -> text.toLowerCase());
         System.out.println(testString4);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
