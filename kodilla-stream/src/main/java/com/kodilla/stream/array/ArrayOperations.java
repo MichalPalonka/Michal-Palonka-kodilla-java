726c5edce6f8b295bac5e5f1e9f1de4a5810f569
MichalPalonka <michalpalonka@gmail.com>
2019-05-05 13:40:00 +0200

    Poprawione nazwy

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,12 +4,16 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
-        IntStream.range(0, numbers.length)
-                .map(n -> numbers[n])
-                .forEach(System.out::println);
-        return IntStream.range(0, numbers.length)
-                .map(n -> numbers[n])
-                .average()
-                .getAsDouble();
+        if (numbers.length > 0) {
+            IntStream.range(0, numbers.length)
+                    .map(n -> numbers[n])
+                    .forEach(System.out::println);
+            return IntStream.range(0, numbers.length)
+                    .map(n -> numbers[n])
+                    .average()
+                    .getAsDouble();
+        } else {
+            return 0;
+        }
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -7,8 +7,8 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage(){
         //Given
-        int numbers [] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-        double testAverage = 10.5;
+        int numbers [] = {};
+        double testAverage = 0;
         //When
         double average = ArrayOperations.getAverage(numbers);
         //Then
