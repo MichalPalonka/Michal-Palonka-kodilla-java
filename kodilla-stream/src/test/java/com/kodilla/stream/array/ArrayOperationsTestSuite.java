2ca7996b53e2cdf7f2a323572069ef80b35a213e
MichalPalonka <michalpalonka@gmail.com>
2019-05-05 13:01:20 +0200

    7.6

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -7,10 +7,11 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage(){
         //Given
-
+        int numbers [] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        double testAverage = 10.5;
         //When
-
+        double average = ArrayOperations.getAverage(numbers);
         //Then
-
+        Assert.assertEquals(testAverage, average, 0.001);
     }
 }
