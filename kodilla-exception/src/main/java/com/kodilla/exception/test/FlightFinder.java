82ea1c6a66cb9e2f4c003e8530a33b9c31c4d4f7
MichalPalonka <michalpalonka@gmail.com>
2019-05-07 22:42:37 +0200

    Zad 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+
+    private final Map<String, Boolean> airports;
+
+    public FlightFinder() {
+        airports = new HashMap<>();
+        airports.put("Warsaw", true);
+        airports.put("Cracow", true);
+        airports.put("Wroclaw", false);
+        airports.put("Poznan", false);
+    }
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        if (airports.containsKey(flight.getArrivalAirport()) && airports.containsKey(flight.getDepartureAirport())) {
+            if(flight.getArrivalAirport() == flight.getDepartureAirport()){
+                throw new RouteNotFoundException("Airplane can't flight to the same Town.");
+            }
+            if(Boolean.TRUE.equals(airports.get(flight.getArrivalAirport()))) {
+                System.out.println("Your flight from " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + "exsist.");
+            }else{
+                throw new RouteNotFoundException("Air plane can't land is this town.");
+            }
+        } else {
+            throw new RouteNotFoundException("The airport does not exist.");
+        }
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -0,0 +1,34 @@
+package com.kodilla.exception.test;
+
+public class FlightFinderRunner {
+
+    public static void main(String[] args) {
+
+        FlightFinder flightFinder = new FlightFinder();
+
+        try { //Lot do tego samego lotniska
+            flightFinder.findFlight(new Flight("Warsaw", "Warsaw"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find flight. \nError code: " + e);
+            System.out.println();
+        }
+        try { //Lot do lotniska do którego nie mozna wylądować (wartość w mapie false)
+            flightFinder.findFlight(new Flight("Warsaw", "Wroclaw"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find flight. \nError code: " + e);
+            System.out.println();
+        }
+        try { //Lot do lotniska którego nie ma w mapie
+            flightFinder.findFlight(new Flight("Warsaw", "Wroclove"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find flight. \nError code: " + e);
+            System.out.println();
+        }
+        try { //Lot który się powiedzie
+            flightFinder.findFlight(new Flight("Warsaw", "Cracow"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find flight. \nError code: " + e);
+            System.out.println();
+        }
+    }
+}
