a9c248dae72d65b2bfbb05a77494d3957c580f88
MichalPalonka <michalpalonka@gmail.com>
2019-06-02 22:29:09 +0200

    infinite recursion fix

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -1,5 +1,10 @@
 package com.kodilla.exception.test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FlightFinderRunner {
 
     public static void main(String[] args) {
@@ -31,24 +36,40 @@ public class FlightFinderRunner {
         }
     }*/
 
-        FlightFinderExpanded flightFinder = new FlightFinderExpanded();
-        try { //Lot do lotniska do którego nie mozna dlecieć bez przesiadki
-            flightFinder.findFlight(new Flight("Warsaw", "Sydney"));
+        final Map<String, List<String>> map = new HashMap<>();
+        map.put("Warsaw", Arrays.asList("Cracow", "Wroclaw"));
+        map.put("Cracow", Arrays.asList("Warsaw", "Wroclaw"));
+        map.put("Wroclaw", Arrays.asList("Warsaw", "Cracow", "Poznan"));
+        map.put("Poznan", Arrays.asList("Wroclaw"));
+
+        FlightFinderExpanded flightFinder = new FlightFinderExpanded(map);
+        try {
+            flightFinder.isFlightPossible("Warsaw", "Cracow");
         } catch (RouteNotFoundException e) {
             System.out.println("Can't find flight. \nError code: " + e);
             System.out.println();
         }
-        try { //Lot do lotniska którego nie ma w mapie
-            flightFinder.findFlight(new Flight("Warsaw", "Wroclove"));
+
+        try {
+            flightFinder.isFlightPossible("Warsaww", "Cracow");
         } catch (RouteNotFoundException e) {
             System.out.println("Can't find flight. \nError code: " + e);
             System.out.println();
         }
-        try { //Lot który się powiedzie
-            flightFinder.findFlight(new Flight("Warsaw", "Dubai"));
+
+        try {
+            flightFinder.isFlightPossible("Warsaw", "Warsaw");
         } catch (RouteNotFoundException e) {
             System.out.println("Can't find flight. \nError code: " + e);
             System.out.println();
         }
+
+        try {
+            flightFinder.isFlightPossible("Warsaw", "Poznan");
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find flight. \nError code: " + e);
+            System.out.println();
+        }
+
     }
 }
