ea42f90ec8d7ec05ce9e84c0726014a19c4cf39d
MichalPalonka <michalpalonka@gmail.com>
2019-10-01 20:42:11 +0200

    24.3 add aspect to fasade

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@EnableAspectJAutoProxy
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("ProcessOrder method call");
+    }
+
+}
